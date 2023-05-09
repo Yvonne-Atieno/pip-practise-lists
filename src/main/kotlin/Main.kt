77a9d1f9b73eeb1c675752a3ee709eaa48fe0be2
Yvonne-Atieno <oyvonneatieno@gmail.com>
2023-05-09 23:40:45 +0300

    first commit

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,57 +1,60 @@
-fun main (){
-    var num=nums(arrayOf(2,4,5,6,7,9,12,15,3))
-    println(num)
-
-greetings()
-
-    var numbers= list(arrayOf(23,45,12,22,26,62))
-    println(numbers)
-
-    digits()
-    sumNum(12,23.2)
-
-
+fun main(){
+    arrayNumber()
+    person()
+    println( names("cynthia angel", "Nickson Wangari", "Mike Awido"))
+    var cars = listOf(
+        Car("ABCD1MN23",1230.0),
+        Car("SHKD70006",269.0),
+        Car("KJDHHNM",261.9),
+        Car("ASJXHNH ",1892.70)
+    )
+    var avgmileage = registration(cars)
+    println("The average mileage is :$avgmileage")
 }
-//No 1
-//Write a function that takes an array of integers as input and returns the sum of all even numbers.
-fun nums(num:Array<Int>):Int{
-    var sum=0
-   for (n in num){
-       if (n%2==0){
-           sum+=n
-       }
-   }
-    return sum
+//1. Create one function that is given the below array:
+//var numbers = arrayOf(32, 17,4,213,78,43,90,31,3,73,11,158,62)
+//(i) prints out the sum of the second and fifth elements (1 point)
+//(ii) prints out the index of 158 (1 point)
+//(iii) prints out the elements in ascending order. (1 point)
+fun arrayNumber(){
+    var numbers = arrayOf(32, 17,4,213,78,43,90,31,3,73,11,158,62)
+    println(numbers[1].plus(numbers[5]))
+    println(numbers.indexOf(158))
+    numbers.sort()
+    println(numbers.contentToString())
 }
-
-
-
-//No 2
-//Write a Kotlin program that prints "Hello, world!" to the console.
-fun greetings(){
-    println("Hello,world")
+//2. Given a list of Person objects, each with the attributes, name, age, height, and weight.
+// sort the list in order of descending age (2 points)
+data class personobjects(var name:String, var age:Int,var height:Double, var weight:Int)
+fun person(){
+    var person1 =personobjects("Yvonne Adhiambo", 20, 1.8,67 )
+    var person2 =personobjects("Mirriam wairinga", 44, 1.9,57 )
+    var person3 =personobjects("cynthia Warukira", 29, 1.8,27 )
+    var Person = listOf(person1, person2, person3)
+    var person = Person.sortedByDescending { Person -> Person.age }
+    println(Person)
 }
-
-//No 3
-//Write a function that takes a list of numbers as a parameter and returns
-// the average of all the numbers in the list
-fun list(numbers:Array<Int>):Double{
-   var avg=numbers.average()
-    return avg
-
+//3. Create a function that takes in 3 names and returns a string array containing all 3 names. (2 points)
+fun names(name:String, name1:String, name2: String):String{
+    var y = (arrayOf(name,name1, name2,))
+    return y.contentToString()
 }
-
-//No 4
-//Write a Kotlin program that uses a loop to print out the numbers 1 through 10.
-fun digits(){
-    for (x in 1..10){
-        println(x)
+//4. Write a function that takes in a list of Car objects each with a registration and
+// mileage attribute and returns the average mileage of all the vehicles in the list. (3 points)
+data class Car(var registration:String,var mileage:Double)
+fun registration(cars:List<Car>):Double{
+    var carMileage = 0.0
+    for(car in cars){
+        carMileage+=car.mileage
     }
+    return carMileage/cars.size
 }
-//No 5
-//Write a Kotlin program that calculates and prints the sum of two numbers.
-fun sumNum(num1:Int,num2:Double){
-    var add=num1+num2
-    println(add)
-}
+
+
+
+
+
+
+
+
 
